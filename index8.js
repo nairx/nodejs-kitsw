6803c627c058fc873e5f7afafb485b21f0c33dd6
nairx <nairxprv@gmail.com>
2026-02-21 15:08:10 +0530

    updated

--- index8.js
+++ index8.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 app.listen(8080);
+app.use(express.json())
 const users = [
   { id: 1, name: "John", email: "john@gmail.com" },
   { id: 2, name: "Mike", email: "mike@gmail.com" },
@@ -10,7 +11,12 @@ app.get("/", (req, res) => {
   res.json(users);
 });
 app.get("/:id", (req, res) => {
-  const id = Number(req.params.id)
+  const id = Number(req.params.id);
   const result = users.find((user) => user.id === id);
   res.json(result);
+});
+app.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.json(users);
 });
