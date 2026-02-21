5cc87b2efe3600d3256974b34c62f778872037f1
nairx <nairxprv@gmail.com>
2026-02-21 11:00:23 +0530

    updated

--- index1.js
+++ index1.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import express from 'express'
+const app = express()
+app.listen(8080,()=>{
+    console.log("Server started")
+})
+//creating first API
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+app.get("/home",(req,res)=>{
+    res.send("This is home API")
+})
